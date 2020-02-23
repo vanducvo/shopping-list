f6f13cae88a80ec29495d7114fe40a74d2db1da1
Duc Vo Van <ducvovan.dev@gmail.com>
2020-02-23 19:00:30 +0700

    load databse done

--- untils/load-db.js
+++ untils/load-db.js
@@ -3,11 +3,12 @@ const fs = require('fs');
 const logger = require('../untils/logger');
 const sqlite3 = require('sqlite3').verbose();
 const appSettings = require('../config/app-settings');
+const utils = require('../untils/untils');
 
-function loadFile (filename){
+function loadFile(filename) {
     return new Promise((resolve, reject) => {
         fs.readFile(filename, 'utf8', (err, data) => {
-            if (err){
+            if (err) {
                 reject(err);
             } else {
                 resolve(data);
@@ -16,11 +17,11 @@ function loadFile (filename){
     })
 }
 
-function createDatabaseFixtures(db){
+function createDatabaseFixtures(db) {
     return new Promise((resolve, reject) => {
         return new Promise((resolve, reject) => {
             logger.info('Dropping all tables database...', 'createDatabaseFixtures()');
-            for (let table_name in appSettings.table_sql){
+            for (let table_name in appSettings.table_sql) {
                 db.run(`DROP TABLE IF EXISTS ${table_name}`)
             }
             logger.info('Droped all tables done!', 'createDatabaseFixtures()');
@@ -56,19 +57,83 @@ function createDatabaseFixtures(db){
     });
 }
 
-function loadDataToDatabase(db){
-    
+function loadDataToDatabase(db, filename, handleRows) {
+    return new Promise((resolve, reject) => {
+        logger.info('Loading database ...', 'loadDataToDatabase()');
+        let reader = fs.createReadStream(filename);
+        let cache = '';
+        reader.on('open', () => {
+            logger.info(`Reading ${filename}`, 'loadDataToDatabase:ReadStream(open)');
+        }).on('data', (chunk) => {
+            logger.debug(`Chunk size: ${chunk.length}`, 'loadDataToDatabase:ReadStream(data)');
+            let actualChunk = cache + chunk;
+            logger.debug(`Actual Chunk  size: ${actualChunk.length}`, 'loadDataToDatabase:ReadStream(data)');
+            let { lines, remainder } = utils.transformChunkIntoLines(actualChunk);
+
+            cache = remainder;
+            for (let line of lines) {
+                handleRows(db, line);
+            }
+        }).on('error', (err) => {
+            logger.error(`Error: ${err.message}`, 'loadDataToDatabase:ReadStream(error)');
+        }).on('close', () => {
+            logger.info(`close file: ${filename}`, 'loadDataToDatabase:ReadStream(close)');
+            resolve();
+        });
+    });
 }
 
-(function mainline (){
+function handleRowsBrands(db, line) {
+    let description = line[1];
+    let manufacturer = line[3] ? line[3] : '';
+    let location = line[4] ? line[4] : '';
+    let website = line[5] ? line[5] : '';
+
+    db.run('INSERT INTO brand (description, manufacturer, location, website) VALUES(?,?,?,?)',
+        description, manufacturer, location, website, (err) => {
+            if (err) {
+                logger.error('INSERT INTO brand ERROR' + err.message +
+                    ' with desciption:' + description + ', manufacturer: ' + manufacturer +
+                    ', location' + location + ', website: ' + website,
+                    'handleRowsBrands:db.run'
+                );
+            }
+        }
+    );
+
+}
+
+function handleRowsItems(db, line) {
+    let upc = line[2];
+    let brandDescription = line[3];
+    let itemDesciption = line[4];
+    db.run('INSERT INTO item (brand_id, upc, description) VALUES ((SELECT id FROM brand WHERE description = ?), ?, ? )',
+        brandDescription, upc, itemDesciption, (err) => {
+            if (err) {
+                logger.error('INSERT INTO item ERROR' + err.message +
+                    ' with upc: ' + upc + ', itemDesciption: ' + itemDesciption +
+                    ', brandDescription: ' + brandDescription,
+                    'handleRowsItems:db.run'
+                );
+            }
+        }
+    );
+}
+
+(function mainline() {
     logger.info(`Script load-bd run at ${new Date().toLocaleDateString()}`, 'mainline()');
     logger.info(`Creating Database: ${appSettings.db_file_name}`, 'mainline()');
     let db = new sqlite3.Database(appSettings.db_file_name);
     let promiseTables = createDatabaseFixtures(db);
     promiseTables.then(() => {
-        console.log('Code Here!');
+        loadDataToDatabase(db, appSettings.brand_file_name, handleRowsBrands).then(() => {
+            logger.info('Load data into brand tables DONE', 'mainline:loadDataToDatabase(resolve)');
+            loadDataToDatabase(db, appSettings.item_file_name, handleRowsItems).then(() => {
+                logger.info('Load data into item tables DONE', 'mainline:loadDataToDatabase(resolve)');
+            });
+        });
     }).catch((err) => {
-        console.log('Code Here');
+        logger.error("Create and Load FAIL, please try again", 'mainline:promiseTables(catch)');
     });
 
     process.on('exit', (code) => {
